a126c16b35010c38e0ffd3d679535cceef0816bd
Tanvir Anowar <tanvir.net@gmail.com>
2021-01-05 11:07:28 +0000

    Initial commit

---
+++ src/IdentityService/IdentityService/Controllers/UserManagementController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserManagementController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UserManagementController> _logger;
+
+        public UserManagementController(ILogger<UserManagementController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return null;
+        }
+    }
+}
